ae5dc7277a2920ce22ce979e37506825054bd5ee
Juan Feris <feris.juan@gmail.com>
2021-06-26 11:18:32 -0500

    Basic graphQL server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from 'express'
+import {graphqlHTTP} from 'express-graphql'
 
 const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true
+}))
 
 app.listen(4000, () => {
     console.log('Listening on port 4000');
