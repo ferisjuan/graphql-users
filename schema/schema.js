fdfa3d19307e01ec8f468de87d33f7eb973b05a2
Juan Feris <feris.juan@gmail.com>
2021-06-27 09:21:19 -0500

    Add json server

--- schema/schema.js
+++ schema/schema.js
@@ -1,33 +1,33 @@
-import {GraphQLInt, GraphQLObjectType, GraphQLSchema, GraphQLString} from 'graphql'
-import _ from 'lodash'
-
-const users = [
-    {id: '23', firstName: 'Juan', age: 43},
-    {id: '23', firstName: 'Mayi', age: 40},
-]
+import {
+    GraphQLInt,
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString
+} from "graphql";
+import _ from "lodash";
 
 const UsertType = new GraphQLObjectType({
-    name: 'User',
+    name: "User",
     fields: {
         id: {type: GraphQLString},
         firstName: {type: GraphQLString},
         age: {type: GraphQLInt}
     }
-})
+});
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
+    name: "RootQueryType",
     fields: {
         user: {
             type: UsertType,
             args: {id: {type: GraphQLString}},
-            resolve(_parentValue, args){
-                return _.find(users, {id: args.id})
+            resolve(_parentValue, args) {
+                return _.find(users, {id: args.id});
             }
         }
     }
-})
+});
 
 export const schema = new GraphQLSchema({
     query: RootQuery
-})
+});
